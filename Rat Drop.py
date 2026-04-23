1fdc43d4e17b490cc8e59df1023bb055c4386fe8
Twitch-jung <l.enzo.artola@gmail.com>
2026-04-22 22:09:22 -0500

    big change
    changed backround to white, made custom size for rat png

--- Rat Drop.py
+++ Rat Drop.py
@@ -4,17 +4,17 @@ import random
 
 pygame.init()
 
-width = 800
-height = 600
+#initializing variables for the screen we are going to use
+
+width = 1000
+height = 800
 size = (width, height)
-black = (0,0,0)
+white = (255, 255, 255)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Rat Click Game")
+pygame.display.set_caption("Rat Clicker Game")
 
 Rat = pygame.image.load("Rat copy.png")
-rat_width = Rat.get_width()
-rat_height = Rat.get_height()
-max_x = max(0, width - rat_width)
+Rat = pygame.transform.scale(Rat, (80, 80))
 
 iterator = 0
 numofRats = 5
@@ -22,13 +22,14 @@ startX = []
 startY = []
 speed = []
 
+#The random rat generator appearing at the top of the screen and falling down
 while iterator < numofRats:
-  startX.append(random.randint(0, max_x))
-  startY.append(-random.randint(rat_height, rat_height * 2))
-  speed.append(0.5)
-  iterator += 1
+    startX.append(random.randint(0, width - Rat.get_width()))
+    startY.append(0 - random.randint(Rat.get_height(), Rat.get_height() * 2))
+    speed.append(0.5)
+    iterator += 1
 
-  # -------------------------
+# -------------------------
 # POWER-UP SETUP
 # -------------------------
 powerup = pygame.Surface((60, 60))
@@ -43,6 +44,7 @@ def increase_speed():
 # -------------------------
 
 replayscreen = False
+
 #Set up game over stuff
 bigfont = pygame.font.SysFont(None, 200)
 playagaintext = bigfont.render("Play Again?", True, (0,200,0))
@@ -55,69 +57,85 @@ notext = smallfont.render("NO", True, (0,200,0))
 nox = width - width/4 - yestext.get_rect().width/2
 
 #Game Loop
-
 gameover = False
 
 while gameover == False:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      gameover = True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True
 
-  #Clicking on the Rats
-  if pygame.mouse.get_pressed()[0]:
-    coords = pygame.mouse.get_pos()
+    #Clicking
+    if pygame.mouse.get_pressed()[0]:
+        coords = pygame.mouse.get_pos()
+
+        # -------------------------
+        # CLICKING THE POWER-UP
+        # -------------------------
+        if powerup_active and replayscreen == False:
+            if (coords[0] >= powerup_x and coords[0] <= powerup_x + 60 and
+                coords[1] >= powerup_y and coords[1] <= powerup_y + 60):
+                increase_speed()
+                powerup_active = False
+        # -------------------------
+
+        if replayscreen == False:
+            iterator = 0
+            while iterator < numofRats:
+                if (coords[0] >= startX[iterator] and
+                    coords[0] <= startX[iterator] + Rat.get_width() and
+                    coords[1] >= startY[iterator] and
+                    coords[1] <= startY[iterator] + Rat.get_height()):
+
+                    startX[iterator] = random.randint(0, width - Rat.get_width())
+                    startY[iterator] = 0 - random.randint(Rat.get_height(), Rat.get_height() * 2)
+                    speed[iterator] = 0.5
+                    break
+                iterator += 1
+
+        else:
+            if coords[0] > yesx and coords[0] < yesx + yestext.get_rect().width and coords[1] > 450 and coords[1] < 450 + yestext.get_rect().height:
+                iterator = 0
+                while iterator < numofRats:
+                    startX[iterator] = random.randint(0, width - Rat.get_width())
+                    startY[iterator] = 0 - random.randint(Rat.get_height(), Rat.get_height() * 2)
+                    speed[iterator] = 0.5
+                    iterator += 1
+                replayscreen = False
+                powerup_active = True  # Reset power-up on replay
+
+            if coords[0] > nox and coords[0] < nox + notext.get_rect().width and coords[1] > 450 and coords[1] < 450 + notext.get_rect().height:
+                gameover = True
+
+    #Updating
     if replayscreen == False:
-      iterator = 0
-      while iterator < numofRats:
-        if coords[0] >= startX[iterator] and coords[0] <= startX[iterator] + Rat.get_width() and coords[1] > startY[iterator] and coords[1] < startY[iterator] + Rat.get_height():
-          startX[iterator] =  random.randint(0, max_x)
-          startY[iterator] = 0 - random.randint(rat_height, rat_height * 2)
-          speed[iterator] = 0.5
-          break
-        iterator += 1
-    else:
-      if coords[0] > yesx and coords[0] < yesx + yestext.get_rect().width and coords[1] > 450 and coords[1] < 450 + yestext.get_rect().height:
         iterator = 0
         while iterator < numofRats:
-          startX[iterator] =  random.randint(0, max_x)
-          startY[iterator] = 0 - random.randint(rat_height, rat_height * 2)
-          speed[iterator] = 0.5
-          iterator +=1
-        replayscreen = False
+            if startY[iterator] + Rat.get_height() > height:
+                replayscreen = True
+                break
+            startY[iterator] += speed[iterator]
+            iterator += 1
 
-      if coords[0] > nox and coords[0] < nox + notext.get_rect().width and coords[1] > 450 and coords[1] < 450 + notext.get_rect().height:
-        gameover = True
+    #Drawing
+    if replayscreen == False:
+        screen.fill(white)
 
+        # Draw Rats
+        iterator = 0
+        while iterator < numofRats:
+            screen.blit(Rat, (startX[iterator], startY[iterator]))
+            iterator += 1
 
-  #Updating
-  if replayscreen == False:
-    iterator = 0
-    #Game over
-    while iterator < numofRats:
-      if startY[iterator] + Rat.get_height() > height:
-        replayscreen = True
-        break
-      startY[iterator] += speed[iterator]
-      iterator += 1
+        # Draw power-up
+        if powerup_active:
+            screen.blit(powerup, (powerup_x, powerup_y))
 
+    else:
+        screen.fill((200,0,0))
+        screen.blit(playagaintext, (pax, 150))
+        screen.blit(yestext, (yesx, 450))
+        screen.blit(notext, (nox, 450))
 
-  #Drawing
-
-  if replayscreen == False:
-    screen.fill(black)
-    iterator = 0
-    while iterator < numofRats:
-      screen.blit(Rat, (startX[iterator], startY[iterator]))
-      iterator += 1
-
-    iterator = 0
-  else:
-    screen.fill((200,0,0))
-
-    screen.blit(playagaintext, (pax, 150))
-    screen.blit(yestext, (yesx, 450))
-    screen.blit(notext, (nox, 450))
-
-  pygame.display.flip()
+    pygame.display.flip()
 
 pygame.display.quit()
